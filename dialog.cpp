132f6ee37233481f2b1fc558e49608246b45199e
SUDHARSON18 <69045980+SUDHARSON18@users.noreply.github.com>
2021-04-04 11:48:32 +0530

    Add files via upload

---
+++ dialog.cpp
@@ -0,0 +1,84 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+#include <QIcon>
+#include <QMessageBox>
+#include <QDebug>
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+void Dialog::on_pushButton_5_clicked(bool checked)
+{
+    QIcon *ico = new QIcon();
+        ico->addPixmap(QPixmap("C:/Users/muthu/Desktop/on.png"),QIcon::Normal,QIcon::On);
+        if(QIcon::On==true) {
+            qDebug() <<"button is checked ";}
+        else
+        {qDebug() <<"button is checked ";}
+        ico->addPixmap(QPixmap("C:/Users/muthu/Desktop/off.png"),QIcon::Normal,QIcon::Off);
+        ui->pushButton_5->setIcon(*ico);
+        ui->pushButton_5->setCheckable(true);
+}
+
+void Dialog::on_pushButton_clicked()
+{
+     QMessageBox::information(this,"c.p.sudharson 212218205058", "This is QMessageBox");
+}
+
+void Dialog::on_pushButton_2_clicked()
+{
+    QMessageBox::StandardButton reply = QMessageBox::question(this,
+                                       "c.p.sudharson 212218205058", "Do You like strawberry?",
+                              QMessageBox::Yes | QMessageBox::No);
+        if(reply == QMessageBox::Yes) {
+            QMessageBox::information(this,"strawberry","yes");
+        }
+        if(reply == QMessageBox::No)
+        {
+            QMessageBox::information(this,"No strawberry","No");
+        }
+}
+
+
+void Dialog::on_pushButton_3_clicked()
+{
+    QMessageBox::StandardButton reply = QMessageBox::question(this,
+                                       "c.p.sudharson 212218205058", "This is Custom message",
+                              QMessageBox::Yes | QMessageBox::NoToAll | QMessageBox::YesToAll | QMessageBox::No);
+        if(reply == QMessageBox::Yes) {
+            qDebug() <<"Nothing is displayed in QMessageBox";
+        }
+        if(reply == QMessageBox::YesToAll)
+        {
+            QMessageBox::warning(this,"c.p.sudharson 212218205058","Display the message with warning as YesToAll");
+        }
+        if(reply == QMessageBox::NoToAll)
+        {
+            QMessageBox::warning(this,"c.p.sudharson 212218205058","Don't display the message with warning as YesToAll");
+        }
+        if(reply == QMessageBox::No) {
+            qDebug() <<"No is clicked";
+        }
+}
+
+
+void Dialog::on_pushButton_4_clicked()
+{
+    QMessageBox::warning(this,"c.p.sudharson 212218205058", "this is warning message");
+
+}
+
+void Dialog::on_pushButton_7_clicked()
+{
+    qDebug() <<"Submitted";
+}
